00cc9ef566f5f63fec21417e36dedb87bd650089
Arogunmasa Abayomi <nomorelimit11@gmail.com>
2024-09-03 15:30:39 +0100

    My first major commit

--- public/index.js
+++ public/index.js
@@ -7,38 +7,40 @@ const submitBtn = document.querySelector('.submit');
 const tbody = document.querySelector('tbody');
 // let checkList = Array.from(tbody?.querySelectorAll('input[type="checkbox"]') as NodeListOf<HTMLInputElement>);
 const headerCheck = document.querySelector('thead input');
+const headerEl = document.querySelector('header');
 class CustomerObj {
-    constructor(name, email, number, date, company, status) {
+    constructor(name, email, number, date, company, status, id) {
         this.name = name;
         this.email = email;
         this.number = number;
         this.date = date;
         this.company = company;
+        this.id = id;
         this.status = status;
     }
 }
 class UI {
-    static showForm() {
-        formContainer === null || formContainer === void 0 ? void 0 : formContainer.classList.add('show-form');
+    static showForm(event) {
+        if (event === showFormBtn) {
+            formContainer === null || formContainer === void 0 ? void 0 : formContainer.classList.add('show-form');
+        }
     }
     static hideForm() {
         formContainer === null || formContainer === void 0 ? void 0 : formContainer.classList.remove('show-form');
         hideFormBtn.classList.add('active');
     }
-    static showActions(event) {
+    static showActions(target) {
         var _a;
-        const target = event;
         const actionLists = document.querySelectorAll('ul');
         actionLists.forEach(list => {
-            list.classList.remove('active');
+            list.classList.remove('active-menu');
         });
         if (target.classList.contains('menu')) {
-            (_a = target.nextElementSibling) === null || _a === void 0 ? void 0 : _a.classList.add('active');
+            (_a = target.nextElementSibling) === null || _a === void 0 ? void 0 : _a.classList.add('active-menu');
         }
     }
-    static checkCustomer(event) {
+    static checkCustomer(target) {
         var _a, _b;
-        const target = event;
         if (target.classList.contains('inp-checkbox') && target.checked) {
             (_a = target.parentElement) === null || _a === void 0 ? void 0 : _a.classList.add('checked');
         }
@@ -57,16 +59,14 @@ class UI {
             headerCheck.checked = false;
         }
     }
-    static deleteCustomer(event) {
+    static deleteCustomer(target) {
         var _a, _b;
-        const target = event;
         if (target.classList.contains('remove')) {
             (_b = (_a = target.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.closest('tr').remove();
         }
     }
-    static duplicateCustomer(event) {
+    static duplicateCustomer(target) {
         var _a, _b, _c;
-        const target = event;
         if (target.classList.contains('duplicate')) {
             const newCopy = (_c = (_b = (_a = target.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.closest('tr')) === null || _c === void 0 ? void 0 : _c.innerHTML;
             const tr = document.createElement('tr');
@@ -74,10 +74,22 @@ class UI {
             tbody === null || tbody === void 0 ? void 0 : tbody.appendChild(tr);
         }
     }
-    static addCustomer(name, email, company, date, number, status) {
+    static deleteChecked(target) {
+        const deleteCheckedBtn = target.classList.contains('remove-customer');
+        if (deleteCheckedBtn) {
+            let checkList = Array.from(tbody === null || tbody === void 0 ? void 0 : tbody.querySelectorAll('input[type="checkbox"]'));
+            checkList.forEach(checked => {
+                var _a, _b, _c;
+                if (checked.checked) {
+                    (_c = (_b = (_a = checked.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement) === null || _c === void 0 ? void 0 : _c.remove();
+                }
+            });
+            headerCheck.checked = false;
+        }
+    }
+    static addCustomer(name, email, company, date, number, status, id) {
         const tr = document.createElement('tr');
         tr.innerHTML = `
-    <tr>
       <td>
         <label class="checkbox">
           <input type="checkbox" class="inp-checkbox" />
@@ -92,6 +104,7 @@ class UI {
       <td>${number}</td>
       <td><span class="status ${status.toLowerCase()}">${status}</span></td>
       <td>${date}</td>
+      <td>${id}</td>
       <td class="menu-bg">
         <button class="menu">
           <svg
@@ -100,21 +113,25 @@ class UI {
             fill="currentColor"
             class="menu-btn">
             <path
-              d="M3 10a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM8.5 10a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM15.5 8.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
+              d="M3 10a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM8.5 10a1.5 1.5 0 
+              1 1 3 0 1.5 1.5 0 0 1-3 0ZM15.5 8.5a1.5 1.5 0 1 0 0 3 1.5 1.5 
+              0 0 0 0-3Z" />
           </svg>
         </button>
-        <ul>
+        <ul class="menu-list">
           <li>
             <button class="edit">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="size-5">
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6">
                 <path
-                  d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
-                <path
-                  d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
               </svg>
               <span>Edit</span>
             </button>
@@ -123,13 +140,15 @@ class UI {
             <button class="remove">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="size-5">
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6">
                 <path
-                  fill-rule="evenodd"
-                  d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z"
-                  clip-rule="evenodd" />
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
               </svg>
               <span>Remove</span>
             </button>
@@ -138,25 +157,36 @@ class UI {
             <button class="duplicate">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="size-5">
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6">
                 <path
-                  d="M7 3.5A1.5 1.5 0 0 1 8.5 2h3.879a1.5 1.5 0 0 1 1.06.44l3.122 3.12A1.5 1.5 0 0 1 17 6.622V12.5a1.5 1.5 0 0 1-1.5 1.5h-1v-3.379a3 3 0 0 0-.879-2.121L10.5 5.379A3 3 0 0 0 8.379 4.5H7v-1Z" />
-                <path
-                  d="M4.5 6A1.5 1.5 0 0 0 3 7.5v9A1.5 1.5 0 0 0 4.5 18h7a1.5 1.5 0 0 0 1.5-1.5v-5.879a1.5 1.5 0 0 0-.44-1.06L9.44 6.439A1.5 1.5 0 0 0 8.378 6H4.5Z" />
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
               </svg>
               <span>Duplicate</span>
             </button>
           </li>
         </ul>
       </td>
-    </tr>
     `;
         tbody === null || tbody === void 0 ? void 0 : tbody.appendChild(tr);
+        UI.allChecked();
     }
 }
-showFormBtn.addEventListener('click', UI.showForm);
+headerEl.addEventListener('click', (e) => {
+    // Show form
+    if (e.target === null)
+        return;
+    const target = e.target;
+    UI.showForm(target);
+    // Remove checked List
+    UI.deleteChecked(target);
+});
+// showFormBtn.addEventListener('click', UI.showForm);
 hideFormBtn.addEventListener('click', UI.hideForm);
 form === null || form === void 0 ? void 0 : form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -169,23 +199,30 @@ form === null || form === void 0 ? void 0 : form.addEventListener('submit', (e) 
     const statsArray = ["Active", "Inactive", "Active", "Inactive"];
     let count = Math.floor(Math.random() * 4);
     let status = statsArray[count];
+    // Add ID
+    let id = Math.ceil(Math.random() * 10000);
+    const IDCount = id.toString().split("");
+    do {
+        id = Math.ceil(Math.random() * 10000);
+    } while ((IDCount.length < 4));
     // Add customer to UI
-    let customer = new CustomerObj(nameInput.value, emailInput.value, companyInput.value, dateInput.value, numberInput.value, status);
-    UI.addCustomer(nameInput.value, emailInput.value, companyInput.value, dateInput.value, numberInput.value, status);
+    let customer = new CustomerObj(nameInput.value, emailInput.value, companyInput.value, dateInput.value, numberInput.value, status, id);
+    UI.addCustomer(nameInput.value, emailInput.value, companyInput.value, dateInput.value, numberInput.value, status, id);
     // Hide form after submitting
     UI.hideForm();
 });
 tbody === null || tbody === void 0 ? void 0 : tbody.addEventListener('click', (e) => {
     if (e.target === null)
         return;
+    const target = e.target;
     // Show actions for each customer
-    UI.showActions(e.target);
+    UI.showActions(target);
     // Check each customer
-    UI.checkCustomer(e.target);
+    UI.checkCustomer(target);
     // Delete Customer
-    UI.deleteCustomer(e.target);
+    UI.deleteCustomer(target);
     // Duplicate Customer
-    UI.duplicateCustomer(e.target);
+    UI.duplicateCustomer(target);
 });
 headerCheck.addEventListener('click', () => {
     let checkList = Array.from(tbody === null || tbody === void 0 ? void 0 : tbody.querySelectorAll('input[type="checkbox"]'));
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const submitBtn = document.querySelector('.submit') as HTMLButtonElement;
 const tbody = document.querySelector('tbody'); 
 // let checkList = Array.from(tbody?.querySelectorAll('input[type="checkbox"]') as NodeListOf<HTMLInputElement>);
 const headerCheck = document.querySelector('thead input') as HTMLInputElement;
+const headerEl = document.querySelector('header') as HTMLElement;
 
 
 type Customer = {
@@ -15,6 +16,7 @@ type Customer = {
   date: string;
   company: string;
   status: string;
+  id: number;
 }
 
 class CustomerObj {
@@ -24,20 +26,25 @@ class CustomerObj {
   date: string;
   company: string;
   status: string;
+  id: number;
 
-  constructor(name: string, email: string, number: string, date: string, company: string, status: string) {
+  constructor(name: string, email: string, number: string, date: string,
+    company: string, status: string, id: number) {
     this.name = name
     this.email = email
     this.number = number
     this.date = date;
     this.company = company
+    this.id = id
     this.status = status
   }
 }
 
 class UI{
-  static showForm(): void {
-    formContainer?.classList.add('show-form');
+  static showForm(event: HTMLElement): void {
+    if (event === showFormBtn) {
+      formContainer?.classList.add('show-form');
+    }
   }
 
   static hideForm(): void {
@@ -45,21 +52,17 @@ class UI{
     hideFormBtn.classList.add('active')
   }
 
-  static showActions(event: EventTarget): void {
-    const target = event as HTMLElement;
-
+  static showActions(target: HTMLElement): void {
     const actionLists = document.querySelectorAll('ul');
     actionLists.forEach(list => {
-      list.classList.remove('active');
+      list.classList.remove('active-menu');
     })
     if (target.classList.contains('menu')) {
-      target.nextElementSibling?.classList.add('active');      
+      target.nextElementSibling?.classList.add('active-menu');      
     }
   }
 
-  static checkCustomer(event: EventTarget): void {
-    const target = event as HTMLInputElement;
-
+  static checkCustomer(target: HTMLInputElement): void {
     if (target.classList.contains('inp-checkbox') && target.checked) {
       target.parentElement?.classList.add('checked');
     } else {
@@ -78,16 +81,13 @@ class UI{
     }
   }
 
-  static deleteCustomer(event: EventTarget): void {
-    const target = event as HTMLButtonElement;
-
+  static deleteCustomer(target: HTMLElement): void {
     if (target.classList.contains('remove')){
       target.parentElement?.parentElement?.closest('tr')!.remove();
     }
   }
 
-  static duplicateCustomer(event: EventTarget): void {
-    const target = event as HTMLButtonElement;
+  static duplicateCustomer(target: HTMLElement): void {
 
     if (target.classList.contains('duplicate')){
       const newCopy = target.parentElement?.parentElement?.closest('tr')?.innerHTML!;
@@ -97,10 +97,23 @@ class UI{
     }
   }
 
-  static addCustomer(name:string, email:string, company:string, date: string, number:string, status: string){
+  static deleteChecked(target: HTMLElement) {
+    const deleteCheckedBtn = target.classList.contains('remove-customer');
+    if (deleteCheckedBtn) {
+      let checkList = Array.from(tbody?.querySelectorAll('input[type="checkbox"]') as NodeListOf<HTMLInputElement>);
+        checkList.forEach(checked => {
+        if (checked.checked) {
+          checked.parentElement?.parentElement?.parentElement?.remove();
+        }
+      })      
+      headerCheck.checked = false;
+      
+    }
+  }
+
+  static addCustomer(name:string, email:string, company:string, date: string, number:string, status: string, id: number){
     const tr = document.createElement('tr') as HTMLTableRowElement;
     tr.innerHTML = `
-    <tr>
       <td>
         <label class="checkbox">
           <input type="checkbox" class="inp-checkbox" />
@@ -115,6 +128,7 @@ class UI{
       <td>${number}</td>
       <td><span class="status ${status.toLowerCase()}">${status}</span></td>
       <td>${date}</td>
+      <td>${id}</td>
       <td class="menu-bg">
         <button class="menu">
           <svg
@@ -123,21 +137,25 @@ class UI{
             fill="currentColor"
             class="menu-btn">
             <path
-              d="M3 10a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM8.5 10a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM15.5 8.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
+              d="M3 10a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM8.5 10a1.5 1.5 0 
+              1 1 3 0 1.5 1.5 0 0 1-3 0ZM15.5 8.5a1.5 1.5 0 1 0 0 3 1.5 1.5 
+              0 0 0 0-3Z" />
           </svg>
         </button>
-        <ul>
+        <ul class="menu-list">
           <li>
             <button class="edit">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="size-5">
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6">
                 <path
-                  d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
-                <path
-                  d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
               </svg>
               <span>Edit</span>
             </button>
@@ -146,13 +164,15 @@ class UI{
             <button class="remove">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="size-5">
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6">
                 <path
-                  fill-rule="evenodd"
-                  d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z"
-                  clip-rule="evenodd" />
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
               </svg>
               <span>Remove</span>
             </button>
@@ -161,26 +181,39 @@ class UI{
             <button class="duplicate">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="size-5">
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6">
                 <path
-                  d="M7 3.5A1.5 1.5 0 0 1 8.5 2h3.879a1.5 1.5 0 0 1 1.06.44l3.122 3.12A1.5 1.5 0 0 1 17 6.622V12.5a1.5 1.5 0 0 1-1.5 1.5h-1v-3.379a3 3 0 0 0-.879-2.121L10.5 5.379A3 3 0 0 0 8.379 4.5H7v-1Z" />
-                <path
-                  d="M4.5 6A1.5 1.5 0 0 0 3 7.5v9A1.5 1.5 0 0 0 4.5 18h7a1.5 1.5 0 0 0 1.5-1.5v-5.879a1.5 1.5 0 0 0-.44-1.06L9.44 6.439A1.5 1.5 0 0 0 8.378 6H4.5Z" />
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
               </svg>
               <span>Duplicate</span>
             </button>
           </li>
         </ul>
       </td>
-    </tr>
     `
     tbody?.appendChild(tr);
+    UI.allChecked();
   }
 }
 
-showFormBtn.addEventListener('click', UI.showForm);
+
+headerEl.addEventListener('click', (e: Event) => {
+  // Show form
+  if (e.target === null) return;
+  const target = e.target as HTMLElement;
+  UI.showForm(target);
+
+  // Remove checked List
+  UI.deleteChecked(target)
+})
+
+// showFormBtn.addEventListener('click', UI.showForm);
 hideFormBtn.addEventListener('click', UI.hideForm);
 
 form?.addEventListener('submit', (e: Event) => {
@@ -194,12 +227,21 @@ form?.addEventListener('submit', (e: Event) => {
 
   // Status update
   const statsArray: string[] = ["Active", "Inactive", "Active", "Inactive"]
-  let count = Math.floor(Math.random() * 4);
-  let status: string = statsArray[count];  
+  let count: number = Math.floor(Math.random() * 4);
+  let status: string = statsArray[count];
+
+  // Add ID
+  let id: number = Math.ceil(Math.random() * 10000);
+  const IDCount = id.toString().split("");
+  do {
+    id = Math.ceil(Math.random() * 10000);
+  } while ((IDCount.length < 4));
 
   // Add customer to UI
-  let customer = new CustomerObj(nameInput.value, emailInput.value, companyInput.value, dateInput.value, numberInput.value, status);
-  UI.addCustomer(nameInput.value, emailInput.value, companyInput.value, dateInput.value, numberInput.value, status)
+  let customer = new CustomerObj(nameInput.value, emailInput.value,
+    companyInput.value, dateInput.value, numberInput.value, status, id);
+  UI.addCustomer(nameInput.value, emailInput.value, companyInput.value,
+    dateInput.value, numberInput.value, status, id)
   
 
   // Hide form after submitting
@@ -208,17 +250,18 @@ form?.addEventListener('submit', (e: Event) => {
 
 tbody?.addEventListener('click', (e: Event) => {
   if (e.target === null) return;
+  const target = e.target as HTMLElement;
   // Show actions for each customer
-  UI.showActions(e.target);
+  UI.showActions(target);
 
   // Check each customer
-  UI.checkCustomer(e.target)
+  UI.checkCustomer(target as HTMLInputElement)
 
   // Delete Customer
-  UI.deleteCustomer(e.target)
+  UI.deleteCustomer(target)
 
   // Duplicate Customer
-  UI.duplicateCustomer(e.target)
+  UI.duplicateCustomer(target)
 })
 
 headerCheck.addEventListener('click', () => {
